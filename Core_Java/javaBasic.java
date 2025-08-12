9926a097b9f729b4199480f12cf98d32bf945ad8
Chaman3212 <chaman.gla_cs24@gla.ac.in>
2025-08-12 14:11:46 +0530

    commit

---
+++ Core_Java/javaBasic.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class javaBasic{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+    //     String input= sc.nextLine();
+    //     System.out.println(input);
+
+    // float integer = sc.nextFloat();
+    // System.out.println(integer);
+    
+
+        //Sum
+    // int a = sc.nextInt();
+    // int b = sc.nextInt();
+    // int sum = a+b;
+    // System.out.println(sum);
+
+    //Product
+//    float rad = sc.nextFloat();
+//    float area = 3.14f *rad*rad;
+//    System.out.println(area);
+    
+        // float a = 25.122f;
+        // int b = (int)a;
+        // System.out.println(b);
+
+        // char a = 'a';
+        // int b = a;
+        // System.out.println(b);
+
+
+        // int tem = sc.nextInt();
+        // int final_temp = tem +273;
+        // System.out.println(final_temp);
+
+        int temp = sc.nextInt();
+        float formula = 9/2;
+        float final_temp = ( temp *formula)+32;
+        System.out.println(final_temp);
+}
+}
